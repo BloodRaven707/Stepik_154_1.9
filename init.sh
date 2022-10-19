e6c52ae311d29c8c24eef6fe2c0c7a5c88a72091
Dmitriy Dzhurabaev <blood.raven.707@gmail.com>
2022-10-19 10:18:51 +0300

    Some fix

--- init.sh
+++ init.sh
@@ -6,3 +6,4 @@ sudo ln -fs /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
